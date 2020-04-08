a763d8c825dd5284e51867439bb815539a72a4dd
Adam <adam.m.leis@gmail.com>
2020-04-08 15:09:48 -0400

    add desc text from live version

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ date_default_timezone_set('America/New_York');
 
 # Error Checking
 if(isset($_GET['errors'])){
-	ini_set('display_errors',1); 
+	ini_set('display_errors',1);
 	error_reporting(E_ALL);
 }
 
@@ -46,7 +46,7 @@ if($isTmach){ // only entered if AJAX calls this page
 		'msprite' 	=> $msprite,
 		'moonage'	=> $md
 	);
-	
+
 	#echo '<pre>'.print_r($get,true).'</pre>';
 	echo json_encode($return);
 } else {
@@ -87,7 +87,7 @@ if($isTmach){ // only entered if AJAX calls this page
 			$gotoDay .= '<option value="'.$x.'">'.$x.'</option>';
 		}
 	}
-	 
+
 	# Clock Numbers
 	$nums = '';
 	for($x=1;$x<=12;$x++){
@@ -154,7 +154,7 @@ if($isTmach){ // only entered if AJAX calls this page
 					<option value="0">Month</option>
 					<?php echo $gotoMonth; ?>
 				</select><!-- End #gotoMonth -->
-		
+
 				<select name="gotoDay" id="gotoDay" class="toTime">
 					<option value="0">Day</option>
 					<?php echo $gotoDay; ?>
@@ -162,14 +162,40 @@ if($isTmach){ // only entered if AJAX calls this page
 
 				<button id="time_travel">Time Travel!</button>
 			</fieldset><!-- End #time_machine -->
-		<div style="display:none">
-			<p>High Life Cosby sweater pariatur, jean shorts Godard reprehenderit aute lo-fi laboris wayfarers direct trade. DIY synth master cleanse, elit odio ennui aesthetic aliqua distillery authentic Vice fugiat. Quinoa nihil cornhole accusamus raw denim Vice meggings Tumblr YOLO, skateboard bicycle rights 8-bit trust fund commodo. Gentrify dreamcatcher yr, hashtag gluten-free accusamus Portland roof party readymade meggings squid post-ironic. Semiotics aliqua Pitchfork bitters yr, nihil esse actually. Lo-fi sartorial sed Tonx officia biodiesel, 8-bit bespoke polaroid Tumblr twee shabby chic. Biodiesel synth cray, selvage duis Godard nesciunt.</p>
-			<p>Nostrud tempor cray, PBR Wes Anderson farm-to-table non beard lo-fi selfies in. Street art occupy consectetur locavore photo booth distillery Neutra Tumblr. Bicycle rights fanny pack messenger bag, Tonx do viral chambray pop-up before they sold out duis trust fund labore. Next level Pitchfork labore, quinoa bespoke tousled raw denim readymade eiusmod actually Tumblr. Lomo veniam Marfa, literally shabby chic scenester nisi kogi Schlitz reprehenderit qui cred you probably haven't heard of them enim pariatur. Cred minim Etsy duis before they sold out, banh mi kitsch. Irony sed sartorial, cupidatat master cleanse mixtape gastropub keffiyeh.</p>
-			<p>Reprehenderit gluten-free viral, est et kitsch kale chips PBR organic freegan pariatur. Drinking vinegar ethical keytar, accusamus American Apparel lo-fi labore aesthetic typewriter do umami sartorial Odd Future pop-up. Brunch qui tempor, retro ethical meggings elit velit 90's aesthetic mumblecore placeat cardigan Intelligentsia. Before they sold out aute Terry Richardson, direct trade cornhole ex tote bag chambray Neutra. Lo-fi vegan banjo, raw denim mumblecore disrupt officia Williamsburg ethnic Marfa American Apparel tote bag. Raw denim eu irure pork belly, cornhole freegan stumptown. Incididunt occaecat hella, McSweeney's biodiesel ex church-key before they sold out tempor sustainable synth Pinterest.</p>
-		</div>
-		<footer>
-			&copy; 2013. Project by <a href="http://adamleis.com" class="external">Adam Leis</a>
-		</footer>
+
+			<article id="info">
+				<h2>Welcome!</h2>
+				<p>Greetings and welcome to Adam&#39;s Astro Clock! The intentions of this project are primarily to educate and demonstrate the wonderful nature of astronomical clocks to any who are interested. It is my hope that you will find this interesting and educational.</p>
+
+				<h2>What are astronomical clocks?</h2>
+				<p>Astronomical clocks do not just tell the time of day. They can tell the time of month and year as well. For time of month, the most &ldquo;natural&rdquo; way, that is to say, the most nature-like way is with the lunar phases.</p>
+
+				<h2>Adam&rsquo;s Astro Clock v1.0</h2>
+				<p>(put this text in separate box next to universal content so that future versions are relevant with text)</p>
+				<p>This clock uses the moon phases to tell what phase the moon is in and, for anyone knowledgeable of lunar phases, the time of month.</p>
+				<blockquote><i>Technical Jargon: </i>This is being calculated using a PHP script translated from Perl by Stephen A. Zarkos. There is some fine tuning that needs done to make it more accurate, but the results so far are pleasing. In the next version, I would like to use SVG to dynamically calculate the placement of the lunar phases. Currently, I am using a PNG image sprite, so I am limited to whatever images I have created already. The only drawback to using SVG as of now is that many mobile devices <u>will not render SVG</u> yet, which is greatly disappointing.</blockquote>
+				<p>The time of year can be displayed as well using the zodiacal constellations. The chosen constellation telling the time of year is whichever constellation is at the meridian with the sun at noon. Knowing the full circle of constellations, ancient astronomers would know what constellation is up at midnight at a given time of year, and be able to deduce that the sun meets the meridian while &ldquo;in&rdquo; the constellation opposite the midnight constellation. The symbols used on Adam&#39;s Astro Clock are the astrological signs, as opposed to the actual constellations, but this is only for representational purposes. I would like for future versions to have the actual start patterns that make up the constellation so that they may be a little more easily recognizable to those who know the constellations better than the zodiac iconography.</p>
+
+				<h2>Why astronomical clocks?</h2>
+				<p>Starting off as vanity projects (to some extent), they showcased a certain builder&rsquo;s expertise and skills in mechanics, planning, and so many other factors that play into building these outstanding devices. If the project was not focusing on the builder, it was showing off the exuberant wealth of the project&rsquo;s patron, because these clocks simply were not cheap.</p>
+				<p>The Gothic era offered more interest in philosophical implications behind the astronomical clocks. In a highly Christian age, people fancied the idea of an ordered cosmos. The astronomical clock was a great representation of how super complex devices must have some governing intelligence behind them. Eventually, these clocks would fade out of focus for a couple of centuries only to be brought back during the Renaissance.</p>
+				<p>During the Renaissance, the astronomical clocks came back into focus as education devices, teaching rather than supporting philosophical notions (primarily, at least). This is when the Prague Astronomical Clock was built, and when many others were built initially. Not many clocks exist, and the Prague clock is easily the oldest surviving. Others have been rebuilt based on specifications.</p>
+
+				<h2>Why I chose to build Adam&#39;s Astro Clock</h2>
+				<p>I first learned about astronomical clocks in a Northern Renaissance art history class. The Prague Astronomical Clock was my first, and it captured my attention immediately. I wrote a report on timekeeping methods and particularly relished the study put into the astronomical clock. These kinds of clocks have a more broad focus than just daily chronometry (time-telling), and almost always focusing on celestial events (hense &quot;astronomical&quot;).</p>
+				<p>At some point early on in my web career, I decided to make a web-based astronomical clock. It would be a few years before I would get serious about it, but that has worked to my benefit. My abilities now are considerably more suitable for the challenge.</p>
+
+				<h2>When were astronomical clocks built?</h2>
+				<p>Astronomical clocks started gaining popularity first in the Gothic Era. The clocks had more of a philosophical use then, as the concept of an ordered universe was appealing to the Christians of the time. In addition to the philosophical value, the clocks did well to bolster the prestige of both the clockmaker &amp; the patron; these clocks were anything but cheap and easy to build.</p>
+				<p>After the clocks began to fade from popularity, a couple centuries passed and the Renaissance Age came, bringing with it new-found interest in the educational value of these extraordinary clocks.</p>
+
+				<h2>Where are there astronomical clocks today?</h2>
+				<p>The oldest surviving astronomical clock is currently in Prague Town Square and was built during the Renaissance Era. Most other clocks do not exist in their original state, but have been recreated based on detailed documentation.</p>
+				<p>One notable clock exists in the Cathedrale Notre-Dame in France. It is the 3<sup>rd</sup> version of the clock. More can be read here [link] on this clock. Another is the Olomouc clock in the Czec Republic, which is a surviving piece from the first years of communism. More details can be studied here [outsideprague.com/Olomouc/astronomical_clock.html].</p>
+				<p>There are some modern astro cock builders today. One hobbyist I discovered [astroclocks.nl] has several versions of actual mechanical clocks he has built. I may be looking to some of his designs for inspiration. Aside from hobbyist clockmakers, astronomical clocks can be purchased at places with astronomy-related products.&nbsp;</p>
+			</article>
+
+			<footer>&copy; 2013. Project by <a href="http://adamleis.com" class="external">Adam Leis</a></footer>
 		</div><!-- End #content -->
 	</div><!-- End #container -->
 
